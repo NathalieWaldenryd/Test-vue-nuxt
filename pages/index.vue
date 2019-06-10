e28e9413e9b88213f31699bae14dd87c3cabd342
Nathalie <nattis190@hotmail.com>
2019-06-10 11:43:11 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="container">
+        <div class="column center">
+          <p class="is-size-4">What does your product do?</p>
+        </div>
+      </div>
+      <div class="container">
+        <div class="column center">
+          <p class="is-size-2">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.?</p>
+        </div>
+      </div>
+      <div class="container">
+        <div class="column center">
+          <p class="is-size-2">Aenean commodo ligula eget dolor.</p>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="column center">
+        <button class="button-is-link">Main CTA</button>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ pages/navigation.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="hero-head">
+    <nav class="navbar har-background-danger">
+      <div class="navbar-brand">
+        <nuxt-link to="/">{{ website }}</nuxt-link>
+      </div>
+      <div class="navbar-menu">
+        <div class="navbar-end">
+          <nuxt-link to="/" class="navbar-item">Home</nuxt-link>
+          <nuxt-link to="/about" class="navbar-item">About</nuxt-link>
+          <nuxt-link to="/contact" class="navbar-item">Contact</nuxt-link>
+        </div>
+      </div>
+    </nav>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  data() {
+    return {
+      website: 'Test'
+    }
+  }
+}
+</script>
